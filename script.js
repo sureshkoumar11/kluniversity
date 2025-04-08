b5c627b04ea758037064c36a891e10ced342945f
Dr KSK <98300792+sureshkoumar11@users.noreply.github.com>
2025-04-08 12:25:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchExcelData() {
     try {
-        const url = "https://raw.githubusercontent.com/sureshkoumar11/Y22_Student_Info/main/data.xlsx";
+        const url = "https://raw.githubusercontent.com/sureshkoumar11/kluniversity/main/data.xlsx";
         const response = await fetch(url);
         if (!response.ok) throw new Error("Failed to fetch Excel file");
 
